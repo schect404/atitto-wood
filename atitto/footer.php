80b421d60262651ad838c5cfe3b786c6a756496c
schect404 <tomilovych@dlit.dp.ua>
2018-10-28 16:01:16 +0200

    Work on comments: some codestyle issues

--- atitto/footer.php
+++ atitto/footer.php
@@ -1,10 +1,13 @@
 <footer>
-<div class="footcomp">
-<div class="row">
 
-<div class="col-md-12 col-xs-12 col-lg-12 col-sm-12" style="text-align:center; margin-left:-9%">
+    <div class="footcomp">
 
-<p><font color='black'><strong> <span style="font-size: 50%; color: black"></span>
+        <div class="row">
+
+            <div class="col-md-12 col-xs-12 col-lg-12 col-sm-12" style="text-align:center; margin-left:-9%">
+
+                <p>
+                    <font color='black'><strong><span style="font-size: 50%; color: black"></span>
 <img src="http://1.bp.blogspot.com/-4njwXch0jpc/Ut6EnB8TCiI/AAAAAAAAEN8/ChxzjoPZImw/s50/Communication-gmail-icon.png" width='3%' height='3%' class="imageslogos">
 <span> atittosk@gmail.com </span><span style="color: black"></span>
 <img src="/telefon4.png" width='3%' height='3%'>
@@ -42,7 +45,8 @@
  <a href="https://www.facebook.com/Atitto-Dreven%C3%A9-domy-s-modernou-izol%C3%A1ciou-441945522814419/?ref=aymt_homepage_panel">
  <p><img src="/facebook.png"  width='30%'  class="imageslogos" > 
  </a></p></span></strong>
-<p>Jánošíkovo nábrežie, 4478, 031 01 Liptovský Mikuláš, Slovenský Republika</p>
+
+    <p>Jánošíkovo nábrežie, 4478, 031 01 Liptovský Mikuláš, Slovenský Republika</p>
 
 </font>
 </div>
