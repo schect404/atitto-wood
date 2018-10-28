732f1123cb573274f65b7c3a005ff498a46d0298
schect404 <tomilovych@dlit.dp.ua>
2018-10-28 15:44:03 +0200

    headers and footers added

---
+++ atitto/main.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+
+<?php include('header.php'); ?>
+
+<div class="row">
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+<div class="col-md-10 col-xs-10 col-lg-10 col-sm-10" >
+<img  src="http://dominant-wood.com.ua/ru/images/topabout.png.pagespeed.ce.0DtPjMe219.png" width='100%' alt="topabout">
+</div>
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+</div>
+
+<div class="row">
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+<div class="col-md-10 col-xs-10 col-lg-10 col-sm-10" >
+<h1 style="text-align: center;">АТІТTО - продукція найвищої якості</h1>
+<p><img  src="/hlav.jpg" width='100%' alt="topabout"></p>
+</div>
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+</div>
+<div class="row">
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+<div class="col-md-5 col-xs-10 col-lg-5 col-sm-5" >
+<p style="text-align: justify;"><strong style="text-align: justify;">Будуємо з: </strong></p>
+<ul style="list-style-type: circle;">
+<li style="text-align: justify;">Подвійного бруса</li>
+<li style="text-align: justify;">Профільованого бруса</li>
+<li style="text-align: justify;">Оциліндрованого бруса</li>
+</ul>
+<p style="text-align: justify;"><strong style="text-align: justify;">Екологічно чисті: </strong></p>
+<ul style="list-style-type: circle;">
+<li style="text-align: justify;">Будинки</li>
+<li style="text-align: justify;">Котеджі</li>
+<li style="text-align: justify;">Комерційні споруди</li>
+<li style="text-align: justify;">Бані</li>
+<li style="text-align: justify;">Альтанки</li>
+</ul>
+<p style="text-align: justify;"><strong>Офіс компанії</strong> знаходиться в Ліптовському Мікулаші, Словаччина. Це місце вибрано невипадково, адже тут століттями традиційно, маючи власну сировину, будували тепле, гарне і міцне житло з деревини. Серед замовників, які прагнуть відпочинку у природних умовах(подалі від міського смороду, пилу та шуму), у наші часи набуває популярність саме помешкання з подвійного, профільованого та оциліндрованого бруса. </p>
+<p style="text-align: justify;"><strong>Помешкання з бруса</strong> мають неабиякі корисні властивості для людини, щодо створення неповторного мікроклімату, завдяки смолам хвойних порід дерев. Наші пращури не мали систем кондиціювання повітря в дерев’яних помешканнях з дерев’яного бруса, але завжди почувалися дуже комфортно завдяки мікроклімату: прохолоді влітку та теплу і затишку взимку. </p>
+<div class="mobiletext">
+<p style="text-align: justify;"><strong style="text-align: justify;">Компанія «АТІТТО»</strong> здійснює мрії, адже проекти, що розробляють наші дизайнери, найкращі! Ви також можете створити свій неповторний проект дому, котеджу, комерційних споруд, бань та альтанок з подвійного, профільованого або оциліндрованого бруса. Наші дизайнери постійно вдосконалюють типові проекти. </p>
+<p style="text-align: justify;"><strong style="text-align: justify;"><br><br> Компанія «АТІТТО»</strong> при створенні індивідуального проекту враховує всі сучасні технології, екологічність, енергоефективність. </p>
+<p style="text-align: justify;"><strong>Наше головна мета</strong> -  задоволення кожного клієнта від початку проектування свого дому, котеджу, комерційної споруди, бані, альтанки з бруса і аж до самого моменту підписання акту прийому передачі готової споруди. Індивідуальне проектування, дизайн внутрішніх приміщень , екстер’єр – все це входить в загальну суму договору і окремо не сплачується. </p>
+<p style="text-align: justify;"><strong>Компанія «АТІТТO»</strong> зібрала під своїм дахом висококваліфікованих фахівців, які постійно вдосконалюють свою майстерність.  Ми впевнені, що вибір нашої компанії буде для вас найкращим, а співробітництво – плідним, довгостроковим та взаємовигідним!</p>
+
+</div>
+</div>
+
+<div class="col-md-5 col-xs-0 col-lg-5 col-sm-5" >
+<div class="comptext">
+<p style="text-align: justify;"><strong style="text-align: justify;">Компанія «АТІТТО»</strong> здійснює мрії, адже проекти, що розробляють наші дизайнери, найкращі! Ви також можете створити свій неповторний проект дому, котеджу, комерційних споруд, бань та альтанок з подвійного, профільованого або оциліндрованого бруса. Наші дизайнери постійно вдосконалюють типові проекти. </p>
+<p style="text-align: justify;"><strong style="text-align: justify;"><br><br> Компанія «АТІТТО»</strong> при створенні індивідуального проекту враховує всі сучасні технології, екологічність, енергоефективність. </p>
+<p style="text-align: justify;"><strong>Наше головна мета</strong> -  задоволення кожного клієнта від початку проектування свого дому, котеджу, комерційної споруди, бані, альтанки з бруса і аж до самого моменту підписання акту прийому передачі готової споруди. Індивідуальне проектування, дизайн внутрішніх приміщень , екстер’єр – все це входить в загальну суму договору і окремо не сплачується. </p>
+<p style="text-align: justify;"><strong>Компанія «АТІТТO»</strong> зібрала під своїм дахом висококваліфікованих фахівців, які постійно вдосконалюють свою майстерність.  Ми впевнені, що вибір нашої компанії буде для вас найкращим, а співробітництво – плідним, довгостроковим та взаємовигідним!</p>
+</div>
+</div>
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+</div>
+<div class="row">
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+<div class="col-md-10 col-xs-10 col-lg-10 col-sm-10" >
+<p><img  src="http://dominant-wood.com.ua/ru/images/bottomabout.png.pagespeed.ce.GY9qhiwrkB.png" width='100%' alt="topabout"></p>
+</div>
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+</div>
+
+<?php include('footer.php'); ?>
+
+</body>
+</html>
---
+++ atitto/o-nas/o-nas.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+
+<?php include('../header.php'); ?>
+
+<div class="row">
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+<div class="col-md-10 col-xs-10 col-lg-10 col-sm-10" >
+
+<p><img  src="http://dominant-wood.com.ua/ru/images/topabout.png.pagespeed.ce.0DtPjMe219.png" width='100%' alt="topabout"></p>
+</div>
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+</div>
+<div class="row">
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+<div class="col-md-10 col-xs-10 col-lg-10 col-sm-10" >
+<h1 style="text-align: center;"> ATITTO - продукція найвищої якості</h1>
+</div>
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+</div>
+<div class="row">
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+<div class="col-md-10 col-xs-10 col-lg-10 col-sm-10" >
+<p><img  src="/hlav.jpg" width='100%' alt="topabout"></p>
+</div>
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+</div>
+<div class="row">
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+<div class="col-md-10 col-xs-10 col-lg-10 col-sm-10" >
+<hr>
+<h1 style="text-align: center; color:black"> <font face="Comic Sans MS"><strong><a href="/UA/o-nas/o-kompanii.html">Про компанію</a></h1></strong></font>
+<hr color="black" size="4">
+
+<a href="/SK/o-nas/o-kompanii.html"><p><img src="/SK/o-nas/o-nas.jpg" width='100%' ></p></a>
+
+
+<hr color="black" size="4">
+
+
+<h1 style="text-align: center; color:black"> <font face="Comic Sans MS"><strong><a href="/UA/o-nas/nashi-partneri.html">Наші партнери</a></h1></strong></font>
+<hr color="black" size="4">
+
+
+<hr color="black" size="4">
+
+
+<h1 style="text-align: center; color:black"> <font face="Comic Sans MS"><strong><a href="/UA/o-nas/dokumenty.html">Документи</a></h1></strong></font>
+<hr color="black" size="4">
+
+
+<hr color="black" size="4">
+
+<h1 style="text-align: center; color:black"> <font face="Comic Sans MS"><strong><a href="/UA/o-nas/nashi-praci.html">Наші роботи</a></h1></strong></font>
+<hr color="black" size="4">
+
+
+
+<hr color="black" size="4">
+
+<h1 style="text-align: center; color:black"> <font face="Comic Sans MS"><strong><a href="/UA/o-nas/recenzia.html">Відгуки</a></h1></strong></font>
+<hr color="black" size="4">
+
+</div>
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+</div>
+<div class="row">
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+<div class="col-md-10 col-xs-10 col-lg-10 col-sm-10" >
+<p><img  src="http://dominant-wood.com.ua/ru/images/bottomabout.png.pagespeed.ce.GY9qhiwrkB.png" width='100%' alt="topabout"></p>
+</div>
+<div class="col-md-1 col-xs-1 col-lg-1 col-sm-1" >
+</div>
+</div>
+
+<?php include('../footer.php'); ?>
+
+</body>
+
+</html>
